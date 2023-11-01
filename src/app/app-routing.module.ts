3989321a6417f05fa8e7844153ef6acc69986fe4
JulioSarauz <jsarauz@29deoctubre.fin.ec>
2023-10-31 21:09:34 -0500

    inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogoComponent } from './catalogo/catalogo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'catalogo'
+  },{
+    path:'catalogo',
+    component:CatalogoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { BannerComponent } from './genericos/banner/banner.component';
+import { CardComponent } from './genericos/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogoComponent,
+    BannerComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
